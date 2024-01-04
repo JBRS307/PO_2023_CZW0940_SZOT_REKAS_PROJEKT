d6027dec14a33563abe234b330e1f6a2af3655d1
kacperszo <kacperszot1@gmail.com>
2024-01-04 10:09:57 +0100

    bug fix

--- darwin/src/main/java/agh/po/darwin/Main.java
+++ darwin/src/main/java/agh/po/darwin/Main.java
@@ -4,6 +4,6 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Application.launch(DarwinApp.class, args);z
+        Application.launch(DarwinApp.class, args);
     }
 }
