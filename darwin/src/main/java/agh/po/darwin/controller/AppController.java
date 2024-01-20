cbd86f3889262ef400dedfcea51dadd6de8ab9ed
JBRS307 <kuba.rekas.307@gmail.com>
2024-01-20 21:09:32 +0100

    saving and reading sim config from file done

--- darwin/src/main/java/agh/po/darwin/controller/AppController.java
+++ darwin/src/main/java/agh/po/darwin/controller/AppController.java
@@ -1,6 +1,11 @@
 package agh.po.darwin.controller;
 
 import agh.po.darwin.model.Simulation;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
 import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
@@ -11,11 +16,18 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class AppController {
+    public ComboBox<String> pickConfig;
     public TextField width;
     public TextField height;
     public TextField startingGrassAmount;
@@ -31,14 +43,26 @@ public class AppController {
     public ComboBox<String> pickMap;
     public ComboBox<String> pickGenome;
     public Button startBtn;
+    public Button saveBtn;
+    public Button readBtn;
+
+    private JSONArray configList;
+    private JSONObject currConfig = new JSONObject();
 
     public void initialize() {
+        readConfigList();
+        setConfigurationComboBox();
+        applyConfig();
         pickMap.getSelectionModel().selectFirst();
         pickGenome.getSelectionModel().selectFirst();
         startBtn.setOnAction(event -> {
             SpawnNewSimulation();
         });
+        saveBtn.setOnAction(event -> {
+            saveConfiguration();
+        });
     }
+
 
     private void SpawnNewSimulation() {
         boolean hell = switch (pickMap.getValue()) {
@@ -100,5 +124,132 @@ public class AppController {
                 }
             }
         });
+    }
+
+    private ObservableList<String> getConfigNames() {
+        JSONObject jo;
+        ObservableList<String> names = FXCollections.observableArrayList();
+        for (int i = 0; i < configList.length(); i++) {
+            jo = (JSONObject) configList.get(i);
+            String joName = jo.getString("name");
+            String name = "";
+            if (Integer.parseInt(jo.getString("counter")) != 0) {
+                name = joName + "/" + jo.getString("counter");
+            } else {
+                name = joName;
+            }
+            names.add(name);
+        }
+        return names;
+    }
+
+    public void setConfigurationComboBox() {
+        var names = getConfigNames();
+        pickConfig.setItems(names);
+    }
+
+    public void readConfigList() {
+        File fp = new File("./src/main/resources/config.json");
+        String fileContent = "";
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            if (!fp.createNewFile()) {
+                Scanner fpScanner = new Scanner(fp);
+                while (fpScanner.hasNextLine()) {
+                    contentBuilder.append(fpScanner.nextLine());
+                }
+                fileContent = contentBuilder.toString();
+                fpScanner.close();
+            }
+        } catch (IOException err) {
+            throw new RuntimeException(err);
+        }
+        if (fileContent.isEmpty()) fileContent = "[]";
+        configList = new JSONArray(fileContent);
+    }
+
+    private JSONObject getConfigByName(String name) {
+        int counter = 0;
+        if (name.contains("/")) {
+            counter = Integer.parseInt(name.substring(name.indexOf('/')+1));
+        }
+        for (int i = 0; i < configList.length(); i++) {
+            JSONObject jo = configList.getJSONObject(i);
+            if (name.equals(jo.getString("name")) &&
+                counter == Integer.parseInt(jo.getString("counter"))) {
+                return jo;
+            }
+        }
+        throw new RuntimeException("No such config");
+    }
+
+    private void setValuesFromConfig() {
+        width.setText(currConfig.getString("width"));
+        height.setText(currConfig.getString("height"));
+        startingGrassAmount.setText(currConfig.getString("startingGrassAmount"));
+        grassEatingEnergy.setText(currConfig.getString("grassEatingEnergy"));
+        grassGrowthPerDay.setText(currConfig.getString("grassGrowthPerDay"));
+        animalsStartAmount.setText(currConfig.getString("animalsStartAmount"));
+        animalsStartEnergy.setText(currConfig.getString("animalsStartEnergy"));
+        fedEnergy.setText(currConfig.getString("fedEnergy"));
+        breedEnergyCost.setText(currConfig.getString("breedEnergyCost"));
+        minMutations.setText(currConfig.getString("minMutations"));
+        maxMutations.setText(currConfig.getString("maxMutations"));
+        genomeLength.setText(currConfig.getString("genomeLength"));
+        pickMap.getSelectionModel().select(currConfig.getString("pickMap"));
+        pickGenome.getSelectionModel().select(currConfig.getString("pickGenome"));
+    }
+
+    private void applyConfig() {
+        pickConfig.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (newValue != null) {
+                    currConfig = getConfigByName(newValue);
+                    setValuesFromConfig();
+                }
+            }
+        });
+    }
+
+    private void currConfigToJSON() {
+        currConfig.clear();
+        currConfig.put("width", width.getText());
+        currConfig.put("height", height.getText());
+        currConfig.put("startingGrassAmount", startingGrassAmount.getText());
+        currConfig.put("grassEatingEnergy", grassEatingEnergy.getText());
+        currConfig.put("grassGrowthPerDay", grassGrowthPerDay.getText());
+        currConfig.put("animalsStartAmount", animalsStartAmount.getText());
+        currConfig.put("animalsStartEnergy", animalsStartEnergy.getText());
+        currConfig.put("fedEnergy", fedEnergy.getText());
+        currConfig.put("breedEnergyCost", breedEnergyCost.getText());
+        currConfig.put("minMutations", minMutations.getText());
+        currConfig.put("maxMutations", maxMutations.getText());
+        currConfig.put("genomeLength", genomeLength.getText());
+        currConfig.put("pickMap", pickMap.getValue());
+        currConfig.put("pickGenome", pickGenome.getValue());
+    }
+
+    private void saveConfiguration() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("save.fxml"));
+        Stage saveStage = new Stage();
+        try {
+            Parent root = loader.load();
+            saveStage.setTitle("Zapisz konfigurację");
+            saveStage.setScene(new Scene(root));
+            saveStage.setResizable(false);
+            saveStage.show();
+        } catch (IOException err) {
+            throw new RuntimeException(err);
+        }
+
+        currConfigToJSON();
+        SaveController saveController = loader.getController();
+        saveController.setCurrConfig(currConfig);
+        saveController.setConfigList(configList);
+        saveController.setApp(this);
+        saveController.initialize();
+
     }
 }
---
+++ darwin/src/main/java/agh/po/darwin/controller/SaveController.java
@@ -0,0 +1,94 @@
+package agh.po.darwin.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SaveController {
+    private AppController app;
+    public Button saveBtn;
+    public Button cancelBtn;
+    public TextField configName;
+    public Label forbiddenChar;
+    private JSONObject currConfig;
+    private JSONArray configList;
+
+    public void initialize() {
+        cancelBtn.setOnAction(event -> {
+            handleCloseButtonAction();
+        });
+        saveBtn.setOnAction(event -> {
+            if(handleSave())
+                handleCloseButtonAction();
+        });
+    }
+
+
+    public void setConfigList(JSONArray ja) {
+        configList = ja;
+    }
+    public void setCurrConfig(JSONObject jo) {
+        currConfig = jo;
+    }
+
+    public void setApp(AppController app) {
+        this.app = app;
+    }
+    private void handleCloseButtonAction() {
+        Stage stage = (Stage) cancelBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    private int getNameCounter(String name) {
+//        System.out.println(configList);
+        int length = configList.length();
+        int maxCounter = -1;
+        for (int i = 0; i < length; i++) {
+            JSONObject jo = (JSONObject) configList.get(i);
+//            System.out.println(jo);
+            if (jo.get("name").equals(name)) {
+                int counter = Integer.parseInt(jo.get("counter").toString());
+                maxCounter = Math.max(counter, maxCounter);
+            }
+        }
+        return maxCounter+1;
+    }
+    private boolean handleSave() {
+        String name = configName.getText();
+        if (name.contains("/")) {
+            forbiddenChar.setVisible(true);
+            return false;
+        }
+        int counter = getNameCounter(name);
+        currConfig.put("name", name);
+        currConfig.put("counter", counter+"");
+
+        configList.put(currConfig);
+
+        try {
+            File fp = new File("./src/main/resources/config.json");
+            if(!fp.exists()) fp.createNewFile();
+
+            FileWriter fpWriter = new FileWriter(fp);
+            BufferedWriter bpWriter = new BufferedWriter(fpWriter);
+            bpWriter.write(configList.toString());
+            bpWriter.close();
+            fpWriter.close();
+        } catch (IOException err) {
+            throw new RuntimeException(err);
+        }
+        app.readConfigList();
+        app.setConfigurationComboBox();
+        return true;
+//        System.out.println(configList);
+    }
+}
